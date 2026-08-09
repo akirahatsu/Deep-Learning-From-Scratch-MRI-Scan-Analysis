a70fb20ca4259690d54368bcfba70a57d1ba4b28
Hatsu_Akira <sardor77nematov@gmail.com>
2026-08-09 15:18:25 +0500

    Refactor CNN class to improve initialization and forward pass

--- src/CNN.py
+++ src/CNN.py
@@ -1,61 +1,82 @@
-class CNN():
+class CNN:
 
-  def __init__(self, kernel_shape):
+  def __init__(self ,kernel_shape = (2,2) , n_kernel = 1):
 
-    self.kernel = np.random.randn(*kernel_shape)
-    self.bias = np.zeros((kernel_shape[0]))
+    self.kernel_shape = kernel_shape
+    self.n_kernel = n_kernel
 
-  def forward(self,input):
+    self.bias = np.zeros((n_kernel))
 
+
+  def forward(self , input):
 
     self.input = np.array(input)
 
-    self.signal_shape = np.array(self.input).shape
 
-    self.output = np.zeros(
-       (self.kernel.shape[0] ,
-       self.signal_shape[0]-self.kernel.shape[1] + 1,
-      self.signal_shape[1]-self.kernel.shape[2] + 1))
+    self.batch ,self.channel , self.height , self.width = self.input.shape
 
-    for k in range(self.kernel.shape[0]):
+    if not hasattr(self, "kernel"):
+      self.kernel = np.random.randn(
+          self.n_kernel,
+          self.channel,
+          *self.kernel_shape
+      )
 
 
 
-        for el in range(self.signal_shape[0]-self.kernel.shape[1] + 1):
-            for i in range(self.signal_shape[1]-self.kernel.shape[2] + 1):
+    self.out_h = self.height - self.kernel.shape[2] + 1
+    self.out_w = self.width - self.kernel.shape[3] + 1
 
-                patch = self.input[
-                     el : el+self.kernel.shape[1] ,
-                     i :i+self.kernel.shape[2]
-                     ]
+    self.output = np.zeros((self.batch , self.n_kernel, self.out_h , self.out_w))
 
-                self.output[k ,el,i] = np.sum(patch * self.kernel[k]) + self.bias[k]
+    for b in range(self.batch):
+      for k in range(self.n_kernel):
+        for h in range(self.out_h):
+          for w in range(self.out_w):
 
-    return self.output
+            total = 0
 
-  def backward(self , dl_dp):
+            for c in range(self.channel):
+              patch = self.input[b,c,
+
+                                h : h + self.kernel.shape[2],
+                                w : w + self.kernel.shape[3]
+                                ]
+              total += np.sum(patch * self.kernel[k,c])
+
+
+
+
+            self.output[b, k, h , w ] = total + self.bias[k]
+
+
+    return   self.output
+
+  def backward(self,dl_dc):
 
     self.dl_dw = np.zeros_like(self.kernel)
-    self.dl_db = np.zeros((self.kernel.shape[0]))
-    self.dl_di = np.zeros_like(self.input, dtype=float) 
+    self.dl_db = np.zeros_like(self.bias)
+    self.dl_di = np.zeros_like(self.input ,dtype= float)
 
-    for k in range(self.kernel.shape[0]):
 
-      for el in range(self.signal_shape[0] - self.kernel.shape[1] + 1):
+    for b in range(self.batch):
+      for k in range(self.n_kernel):
+        for h in range(self.out_h):
+          for w in range(self.out_w):
 
-        for i in range(self.signal_shape[1] - self.kernel.shape[2] + 1) :
+            for c in range(self.channel):
 
-          patch = self.input[
-            el : el + self.kernel.shape[1] ,
-            i :i + self.kernel.shape[2]
-            ]
+              patch = self.input[b,c,
 
-          self.dl_dw[k] += patch * dl_dp[k,el,i]
-          self.dl_db[k] += dl_dp[k,el,i]
+                                h : h + self.kernel.shape[2],
+                                w : w + self.kernel.shape[3]
+                                ]
+              self.dl_dw[k,c] += patch * dl_dc[b,k,h,w]
 
-          self.dl_di[
-            el : el + self.kernel.shape[1] ,
-            i :i + self.kernel.shape[2]
-            ] += self.kernel[k] * dl_dp[k,el,i]
+              self.dl_db[k] += dl_dc[b,k,h,w]
+              
+              self.dl_di[b,c,
+                    h : h + self.kernel.shape[2],
+                    w : w + self.kernel.shape[3]] += self.kernel[k,c] * dl_dc[b,k,h,w]
 
-    return  self.dl_di
+    return self.dl_di
