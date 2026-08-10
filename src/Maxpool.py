2c8237b7c266bf692d9aa9d48e0e0ac395a16e59
Hatsu_Akira <sardor77nematov@gmail.com>
2026-08-10 15:38:39 +0500

    Update Maxpool.py

--- src/Maxpool.py
+++ src/Maxpool.py
@@ -1,36 +1,60 @@
-import numpy as np
+import numpy as np 
 
-class MaxPool():
 
-  def __init__(self , pool_size = (2,2) ,stride  = 2) :
+class Maxpooling:
 
-    self.pool_h = pool_size[0]
-    self.pool_w  =  pool_size[1]
+  def __init__(self , pool_size = 2 , stride = 2):
+    self.pool_size = pool_size
     self.stride = stride
 
-  def forward(self,input):  
+  def forward(self , input):
 
-    batch , channel , height , width = input.shape
+    self.input = input
+    batch ,channel , height , width = input.shape
 
-    output_h = (height - self.pool_h) // stride + 1
-    output_w = (width - self.pool_w) // stride + 1
+    out_h = (height - self.pool_size) // self.stride + 1
+    out_w = (width - self.pool_size) // self.stride + 1
 
-    self.output = np.zeros((batch , channel , output_h , output_w))
-    self.max_index =  np.zeros((batch , channel , output_h ,output_w , 2) , dtype=int)
-
+    self.output = np.zeros((batch , channel , out_h ,out_w))
+    self.max_index = np.zeros((batch , channel , out_h , out_w ,2))
 
     for b in range(batch):
       for c in range(channel):
-        for h in range(0 , (height - pool_h) + 1 , stride) :
-          for w in range(0 , width - pool_w + 1 , stride):
+        for h in range(0 , (height - self.pool_size) + 1 , self.stride):
+          for w in range(0 , (width - self.pool_size) + 1 , self.stride):
 
-            patch = input[b,c,h :h +pool_h , w : w + pool_w]
+            patch = input[b,c ,
+                                h : h + self.pool_size ,
+                                w : w + self.pool_size]
 
-            self.output[b , c , h//stride , w//stride] = np.max(patch)
+            self.output[b,c,h//self.stride, w//self.stride] = np.max(patch)
+
+            max_pos = np.unravel_index(np.argmax(patch) ,patch.shape)
+
+            self.max_index[b,c,h//self.stride ,w//self.stride] = (max_pos[0] ,max_pos[1])
+
+    return self.output
+
+
+  def backward(self , dl_dp):
+
+    self.dl_di = np.zeros_like(self.input ,dtype=float)
+    batch , channel  , height , width = self.input.shape
+    mask = np.zeros((self.pool_size , self.pool_size))
+
+    for b in range(batch):
+      for c in range(channel):
+        for h in range(0 ,(height - self.pool_size) + 1 ,self.stride):
+          for w in range(0 ,( width - self.pool_size) + 1, self.stride):
+
+            max_pos_x , max_pos_y = self.max_index[b,c,h//self.stride ,w//self.stride]
             
-            max_pos = np.unravel_index(np.argmax(patch) , patch.shape)
-
-            self.max_index[b,c,h//stride , w//stride] = (h + max_pos[0] , w + max_pos[1])
-
-
-    return self.output   , self.max_index  
+            mask[int(max_pos_x) ,int(max_pos_y)] = 1
+            self.dl_di[
+                        b, c,
+                        h:h+self.pool_size,
+                        w:w+self.pool_size
+                 ] += mask * dl_dp[b, c, h//self.stride, w//self.stride]
+                                
+   return self.dl_di
+  
